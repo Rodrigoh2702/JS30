f3498d4788a6464b1c4906f9c8d23ae739515b0e
Rodrigo Hernández Góngora <rodrigoh.gongora@gmail.com>
2020-08-20 21:48:10 -0500

    Finished the second exercise

--- 2-JS and CSS clock/main.js
+++ 2-JS and CSS clock/main.js
@@ -1,5 +1,18 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
-  console.log('Hi');
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsInDegrees = ((seconds / 60) * 360) + 90;
+  const minutesInDegrees = ((minutes / 60) * 360) + 90;
+  const hoursInDegrees = ((hours / 24) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsInDegrees}deg)`;
+  minuteHand.style.transform = `rotate(${minutesInDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursInDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
